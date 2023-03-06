8dacfd263b394617014716032db8433319dff1e5
Derlis Cuba <d.erliscuba14@hotmail.com>
2023-03-05 21:38:11 -0300

    First Commit

---
+++ to-do-1.js
@@ -0,0 +1,26 @@
+// 1
+let myNumber = 42;
+let myName = "Derlis";
+
+let temp = myNumber;
+myNumber = myName;
+myName = temp;
+
+console.log(myNumber);
+console.log(myName);
+
+// 2
+for (let i = -52; i < 1067; i++) {
+  console.log(i);
+}
+
+// 3
+function beCheerful() {
+  for (let i = 0; i < 99; i++) {
+    console.log("Buenos dias!");
+  }
+}
+
+//beCheerful();
+
+// 4
