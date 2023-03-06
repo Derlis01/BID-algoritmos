eae8d0bcf63aacd22eceac8e5c4119ff0c785898
Derlis Cuba <d.erliscuba14@hotmail.com>
2023-03-06 00:04:39 -0300

    Agregando tareas hasta el 12

--- to-do-1.js
+++ to-do-1.js
@@ -1,26 +1,116 @@
 // 1
-let myNumber = 42;
-let myName = "Derlis";
+// let myNumber = 42;
+// let myName = "Derlis";
 
-let temp = myNumber;
-myNumber = myName;
-myName = temp;
+// let temp = myNumber;
+// myNumber = myName;
+// myName = temp;
 
-console.log(myNumber);
-console.log(myName);
+// console.log(myNumber);
+// console.log(myName);
 
 // 2
-for (let i = -52; i < 1067; i++) {
-  console.log(i);
-}
+// for (let i = -52; i < 1067; i++) {
+//   console.log(i);
+// }
 
 // 3
-function beCheerful() {
-  for (let i = 0; i < 99; i++) {
-    console.log("Buenos dias!");
-  }
-}
+// function beCheerful() {
+//   for (let i = 0; i < 99; i++) {
+//     console.log("Buenos dias!");
+//   }
+// }
 
 //beCheerful();
 
 // 4
+// for (let i = -300; i < 1; i++) {
+//   if (i % 3 === 0) {
+//     if (i !== -3 && i !== -6) {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// 5
+// let counter = 2000;
+// let interruptor = true;
+
+// while (interruptor) {
+//   counter++;
+//   console.log(counter);
+//   if (counter >= 5280) {
+//     interruptor = false;
+//   }
+// }
+
+// 6
+// let dia = 14;
+// let fecha = 01;
+
+// if (dia === 14 && fecha === 01) {
+//   console.log("Como lo supiste!!!");
+// } else {
+//   console.log("Un dia cualquiera...");
+// }
+
+// 7
+// function yearbi(year) {
+//   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//     console.log(`${year} es una bisiesto!`);
+//   }
+// }
+
+// yearbi(2024);
+
+// 8
+// let counter = 0;
+
+// for (let i = 512; i < 4097; i++) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//     counter++;
+//   }
+
+//   if (i === 4096) {
+//     console.log(counter);
+//   }
+// }
+
+// 9
+// let counter = 0;
+// let interruptor = true;
+
+// while (interruptor) {
+//   counter++;
+
+//   if (counter % 6 === 0) {
+//     console.log(counter);
+//   }
+
+//   if (counter === 60000) {
+//     interruptor = false;
+//   }
+// }
+
+// 10
+// for (let i = 1; i < 101; i++) {
+//   if (i % 5 !== 0 && i % 10 !== 0) {
+//     console.log(i);
+//   }
+
+//   if (i % 5 === 0) {
+//     console.log("Codificando");
+//   }
+
+//   if (i % 10 === 0) {
+//     console.log("Dojo");
+//   }
+// }
+
+// 11
+// function ShowMessage(incoming) {
+//   console.log(incoming);
+// }
+
+// 12
